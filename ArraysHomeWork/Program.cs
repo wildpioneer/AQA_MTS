561554bb1c5b2873ea3578cbb0c0a19437ffaa49
Aleksandr Trostyanko <atrostynko@gmail.com>
2023-11-28 22:04:56 +0100

    Added HomeWork

---
+++ ArraysHomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ArraysHomeWork;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Task1 task1 = new Task1();
+        task1.Execute();
+
+        Task2 task2 = new Task2();
+        task2.Execute();
+
+        Task3 task3 = new Task3();
+        task3.Execute();
+
+        Task4 task4 = new Task4();
+        task4.Execute();
+
+        Task5 task5 = new Task5();
+        task5.Execute();
+
+        Task6 task6 = new Task6();
+        task6.Execute();
+        
+        Task7 task7 = new Task7();
+        task7.Execute();   
+
+        Task8 task8 = new Task8();
+        task8.Execute();
+        
+        Task9 task9 = new Task9();
+        task9.Execute();
+        
+        Task10 task10 = new Task10();
+        task10.Execute();
+
+        Task11 task11 = new Task11();
+        task11.Execute();
+        */
+        
+        Task12 task12 = new Task12();
+        task12.Execute();
+    }
+}
