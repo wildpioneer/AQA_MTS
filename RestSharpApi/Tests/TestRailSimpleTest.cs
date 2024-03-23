bdb377d958b22b26925a28b54891c4f4323ec1fb
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-03-23 11:41:04 +0100

    Update

--- RestSharpApi/Tests/TestRailSimpleTest.cs
+++ RestSharpApi/Tests/TestRailSimpleTest.cs
@@ -1,7 +1,9 @@
 using System.Net;
+using System.Text.Json;
 using NLog;
 using RestSharp;
 using RestSharp.Authenticators;
+using RestSharpApi.Models;
 
 namespace RestSharpApi.Tests;
 
@@ -60,6 +62,95 @@ public class TestRailSimpleTest
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 
+    [Test]
+    public void SimplePostTest()
+    {
+        const string endpoint = "index.php?/api/v2/add_project";
 
-    
+        Dictionary<string, object> json = new Dictionary<string, object>();
+        json.Add("name", "WP Project 2");
+        json.Add("announcement", "Test project description");
+        json.Add("show_announcement", true);
+        json.Add("suite_mode", 3);
+        
+        var options = new RestClientOptions(BaseRestUri)
+        {
+            Authenticator = new HttpBasicAuthenticator("atrostyanko@gmail.com", "Qwertyu_1")
+        };
+            
+        // Setup Rest Client
+        var client = new RestClient(options);
+        
+        // Setup Request
+        Logger.Info(JsonSerializer.Serialize(json));
+        var request = new RestRequest(endpoint)
+            .AddJsonBody(JsonSerializer.Serialize(json));
+        /*
+         * Content-Type
+         * RestSharp will use the correct content type by default. Avoid adding the Content-Type header manually
+         * to your requests unless you are absolutely sure it is required. You can add a custom content type to the body parameter itself.
+         */
+
+        // Execute Request
+        var response = client.ExecutePost(request);
+        
+        Logger.Info(response.Content);
+        
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        
+        // Преобразуем тело ответа в объект JSON
+        dynamic responseObject = Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
+        
+        // Получаем значение поля "id"
+        int id = responseObject!.id;
+        
+        // Используем значение "id" по своему усмотрению
+        Logger.Info($"Значение поля 'id': {id}");
+    }
+
+    [Test]
+    public void AdvancedPostTest()
+    {
+        const string endpoint = "index.php?/api/v2/add_project";
+
+        Project expectedProject = new Project
+        {
+            Name = "WP Project 2",
+            Announcement = "Test project description",
+            IsShowAnnouncement = true,
+            SuiteMode = 2
+        };
+        
+        var options = new RestClientOptions(BaseRestUri)
+        {
+            Authenticator = new HttpBasicAuthenticator("atrostyanko@gmail.com", "Qwertyu_1")
+        };
+            
+        // Setup Rest Client
+        var client = new RestClient(options);
+        
+        // Setup Request
+        var request = new RestRequest(endpoint).AddJsonBody(expectedProject);
+        
+        // Execute Request
+        var response = client.ExecutePost<Project>(request);
+        Project actualProject = response.Data;
+
+        Logger.Info(actualProject);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            
+            Assert.That(actualProject.Name, Is.EqualTo(expectedProject.Name));
+            
+            Assert.That(actualProject.Equals(expectedProject));
+        });
+        
+        // Получаем значение поля "id"
+        int id = actualProject.Id;
+        
+        // Используем значение "id" по своему усмотрению
+        Logger.Info($"Значение поля 'id': {id}");
+    }
 }
