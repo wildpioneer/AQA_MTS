7473d5c2de187475c76922d6f535917585812583
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-03-28 14:17:24 +0100

    Updated

--- RestSharpApi/Tests/SchemaTest.cs
+++ RestSharpApi/Tests/SchemaTest.cs
@@ -11,7 +11,7 @@ namespace RestSharpApi.Tests;
 public class SchemaTest
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-    
+
     private const string BaseRestUri = "https://aqa2504.testrail.io/";
 
     [Test]
@@ -26,24 +26,24 @@ public class SchemaTest
             IsShowAnnouncement = true,
             SuiteMode = 2
         };
-        
+
         // Загрузка JSON-схемы из файла
         string schemaJson = File.ReadAllText(@"Resources/schema.json");
-        
+
         // Создем экземпляр JSON-схемы
-        JSchema schema = JSchema.Parse(schemaJson); 
-        
+        JSchema schema = JSchema.Parse(schemaJson);
+
         var options = new RestClientOptions(BaseRestUri)
         {
             Authenticator = new HttpBasicAuthenticator("atrostyanko@gmail.com", "Qwertyu_1")
         };
-            
+
         // Setup Rest Client
         var client = new RestClient(options);
-        
+
         // Setup Request
         var request = new RestRequest(endpoint).AddJsonBody(expectedProject);
-        
+
         // Execute Request
         var response = client.ExecutePost(request);
 
@@ -52,9 +52,31 @@ public class SchemaTest
         {
             // Получаем тело ответа в виде JObject
             JObject responseData = JObject.Parse(response.Content);
-            
+
             // Проверка соответствия ответа JSON-схеме
             Assert.That(responseData.IsValid(schema));
         }
+    }
+
+    [Test]
+    public void SchemaTest1()
+    {
+        string schemaJson = @"{
+            'description': 'A person',
+            'type': 'object',
+            'properties':
+                {
+                    'name': {'type':'string'},
+                    'hobbies': {
+                        'type': 'array',
+                        'items': {'type':'string'}
+                    }
+                }
+        }";
+
+        JsonSchema schema = JsonSchema.Parse(schemaJson);
+
+        JObject person = JObject.Parse(@"{'name': 'James','hobbies': ['.NET', 'Blogging', 'Reading', 'Xbox', 'LOLCATS']}");
+        Assert.That(person.IsValid(schema));
     }
 }
