b5f219dfb25cd1e1f8103d3bb4ca070ff3d9a5da
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-04-06 13:39:29 +0200

    Updated

--- TestRail/Core/DriverFactory.cs
+++ TestRail/Core/DriverFactory.cs
@@ -15,6 +15,7 @@ public class DriverFactory
         chromeOptions.AddArguments("--disable-gpu");
         chromeOptions.AddArguments("--disable-extensions");
         chromeOptions.AddArguments("--headless");
+        chromeOptions.AddArguments("--remote-debugging-pipe");
 
         chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
         chromeOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);
