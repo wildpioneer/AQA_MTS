d35e10a78274275144a642440ee469780c4d0715
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-04-06 12:18:15 +0200

    Allure added

--- TestRail/Tests/API/BaseApiTest.cs
+++ TestRail/Tests/API/BaseApiTest.cs
@@ -1,9 +1,12 @@
+using Allure.NUnit;
 using NLog;
 using TestRail.Clients;
 using TestRail.Services;
 
 namespace TestRail.Tests.API;
 
+[Parallelizable(scope: ParallelScope.Fixtures)]
+[AllureNUnit]
 public class BaseApiTest
 {
     private readonly Logger _logger = LogManager.GetCurrentClassLogger();
